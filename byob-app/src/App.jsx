d9fd17ccc23f3f249624c0aa5c5f3189cc6e2dbc
Neil515 <slow3605@gmail.com>
2025-07-08 20:45:27 +0800

    Create App.jsx

---
+++ byob-app/src/App.jsx
@@ -0,0 +1,12 @@
+import ByobCardPreview from "./components/ByobCardPreview";
+
+function App() {
+  return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">🍷 台北 BYOB 餐廳清單</h1>
+      <ByobCardPreview />
+    </div>
+  );
+}
+
+export default App;
