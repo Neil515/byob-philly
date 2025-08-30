378926dccf071f510964514e020d4cfe25f7e446
Neil515 <slow3605@gmail.com>
2025-08-30 09:51:03 +0800

    Update page-restaurant-join-us.php

--- wordpress/page-restaurant-join-us.php
+++ wordpress/page-restaurant-join-us.php
@@ -12,8 +12,8 @@ get_header(); ?>
     <div class="container">
         <!-- 頁面標題和描述 -->
         <div class="page-header">
-            <h1 class="page-title">餐廳直接加入</h1>
-            <p class="page-description">歡迎加入 BYOB 平台！請填寫以下資料，我們將為您建立餐廳頁面。</p>
+            <h1 class="page-title">餐廳加入</h1>
+            <p class="page-description">歡迎加入 BYOB 地圖！請填寫以下資料，我們將為您建立餐廳頁面。</p>
         </div>
 
         <!-- 表單容器 - 將在 UXBuilder 中插入表單 -->
