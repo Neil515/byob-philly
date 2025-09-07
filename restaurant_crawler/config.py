87c90a13f074594b7be21ce058823c00d8fffa80
Neil515 <slow3605@gmail.com>
2025-09-07 18:58:35 +0800

    Create config.py

---
+++ restaurant_crawler/config.py
@@ -0,0 +1,10 @@
+# Google Places API 設定
+# 請將 your_api_key_here 替換為您的實際API金鑰
+GOOGLE_PLACES_API_KEY = "AIzaSyCzcNzweMQjo_vbspHtQcHGmWqhE2GDwjg"
+
+# 搜尋設定
+SEARCH_QUERY = "台北 燒烤"
+MAX_RESULTS = 8
+
+# 輸出設定
+OUTPUT_FILE = "restaurant_data_台北_燒烤_20250907_185444.xlsx"
