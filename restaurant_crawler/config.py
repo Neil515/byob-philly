dcacbeb1524ecf169a051b24861d2dc88ec89f06
Neil515 <slow3605@gmail.com>
2025-09-10 15:44:39 +0800

    Update config.py

--- restaurant_crawler/config.py
+++ restaurant_crawler/config.py
@@ -3,8 +3,8 @@
 GOOGLE_PLACES_API_KEY = "AIzaSyCzcNzweMQjo_vbspHtQcHGmWqhE2GDwjg"
 
 # 搜尋設定
-SEARCH_QUERY = "台北 燒烤"
-MAX_RESULTS = 8
+SEARCH_QUERY = "台北 麻辣鍋"
+MAX_RESULTS = 50
 
 # 輸出設定
-OUTPUT_FILE = "restaurant_data_台北_燒烤_20250907_185444.xlsx"
+OUTPUT_FILE = "restaurant_data_台北_麻辣鍋_20250910_135138.xlsx"
